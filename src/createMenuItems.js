81eb6538d1cc18e8cc6a9d062c0a3d9ce67cd469
Richard Lasco <lasco9@gmail.com>
2022-05-05 15:54:41 -0500

    Further changes to modules and styling

--- src/createMenuItems.js
+++ src/createMenuItems.js
@@ -1,14 +1,16 @@
-function createCookies(name, price) {
+function createCookies(name, price, image) {
     const cookieDiv = document.createElement('div');
     const cookieHeader = document.createElement('h4');
     const cookieImage = document.createElement('img');
     const cookiePara = document.createElement('p');
     const cookiePricePara = document.createElement('p');
     cookieDiv.classList.add('menu-item');
+    cookieHeader.classList.add('menu-item-header');
     cookiePara.classList.add('menu-item-desc');
+    cookiePricePara.classList.add('menu-item-price');
+    cookieImage.setAttribute('src', `${image}`);
     cookieHeader.textContent = name;
     cookiePricePara.textContent = price;
-    cookiePricePara.classList.add('menu-item-price');
     cookieDiv.appendChild(cookieHeader);
     cookieDiv.appendChild(cookieImage);
     cookieDiv.appendChild(cookiePara);
@@ -16,7 +18,7 @@ function createCookies(name, price) {
     return cookieDiv;
 }
 
-function createDips(name, price) {
+function createDips(name, price, image) {
     const dipDiv = document.createElement('div');
     const dipHeader = document.createElement('h4');
     const dipImage = document.createElement('img');
@@ -24,6 +26,7 @@ function createDips(name, price) {
     const dipPricePara = document.createElement('p');
     dipDiv.classList.add('menu-item');
     dipPara.classList.add('menu-item-desc');
+    dipImage.setAttribute('src', `${image}`);
     dipHeader.textContent = name;
     dipPricePara.textContent = price;
     dipPricePara.classList.add('menu-item-price');
